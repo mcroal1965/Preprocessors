f69e143d1e9b0d717adacb7fa74aca49348c3945
Brent Croal <brent.croal@agile-banking.com>
2020-04-07 14:37:20 -0400

    EncompassDIPprep

---
+++ RemoveDoubleQuotes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace RemoveDoubleQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileNameIn;
+            string FileNameOut;
+
+            FileNameIn = args[0];
+            FileNameOut = args[1];
+
+            try
+            {
+                StreamReader reading = File.OpenText(FileNameIn);
+                string str;
+
+                while ((str = reading.ReadLine()) != null)
+                {
+                    File.AppendAllText(FileNameOut, str.Replace("\"", "") + "\r\n");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex);
+
+            }
+        }
+    }
+}
+
---
+++ RemoveFirstLine/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace RemoveFirstLine
+{
+    class Program{
+        static void Main(string[] args)
+        {
+            Int32 Counter;
+
+            string FileNameIn;
+            string FileNameOut;
+
+            FileNameIn = args[0];
+            FileNameOut = args[1];
+
+            try
+            {
+                StreamReader reading = File.OpenText(FileNameIn);
+                string str;
+
+                Counter = 0;
+
+                while ((str=reading.ReadLine()) != null)
+                {
+                    if (Counter > 0)
+                    {
+                        File.AppendAllText(FileNameOut, str + "\r\n");
+                    }
+
+                    Counter += 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex);
+            }
+        }
+    }
+}
