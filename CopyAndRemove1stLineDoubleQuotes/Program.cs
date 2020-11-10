4e3148670ff2cec0740db72a2874a8628f732575
Brent Croal <brent.croal@agile-banking.com>
2020-11-10 11:33:19 -0500

    brents changes

---
+++ CopyAndRemove1stLineDoubleQuotes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace CopyAndRemove1stLineDoubleQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 Counter;
+
+            string FileNameIn;
+            string FileNameOut;
+
+            FileNameIn = args[0];
+            FileNameOut = args[1];
+
+            try
+            {
+                StreamReader reading = File.OpenText(FileNameIn);
+                string str;
+
+                Counter = 0;
+
+                while ((str = reading.ReadLine()) != null)
+                {
+                    if (Counter > 0)
+                    {
+                        File.AppendAllText(FileNameOut, str.Replace("\"", "") + "\r\n");
+                    }
+
+                    Counter += 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex);
+            }
+        }
+    }
+}
